3fcf112b3ef74508c4cfd80b8775873dfdee0a56
sukhangnt3 <sukhangnt3@gmail.com>
2024-08-16 16:33:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let menuVisible = false;
+//menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("cisco");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("cms");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("creatividad");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
